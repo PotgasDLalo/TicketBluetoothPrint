f9636684d69976c1c6b73f554d3bafd6bcde240a
Eduardo <lalo_lopez23@outlook.com>
2022-12-12 15:38:04 -0700

    1er

--- app/src/main/java/com/eduardo/ticketbluetoothprint/MainActivity.kt
+++ app/src/main/java/com/eduardo/ticketbluetoothprint/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.eduardo.ticketbluetoothprint.databinding.ActivityMainBinding
 import com.mazenrashed.printooth.Printooth
 import com.mazenrashed.printooth.data.printable.Printable
@@ -43,6 +44,7 @@ class MainActivity : AppCompatActivity(), PrintingCallback {
 
     private var printing: Printing? = null
     private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
